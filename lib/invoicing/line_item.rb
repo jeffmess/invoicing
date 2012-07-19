8f3285f05abd9b3b81645d7362a9fde5d67be6fe
Jeffrey van Aswegen <jeffmess@gmail.com>
2012-07-19 10:50:14 +0200

    Mass assignmnet issues addressed.

--- lib/invoicing/line_item.rb
+++ lib/invoicing/line_item.rb
@@ -2,6 +2,8 @@ module Invoicing
   class LineItem < ActiveRecord::Base
     belongs_to :invoice
     belongs_to :invoiceable, polymorphic: true
+
+    attr_accessible :invoiceable, :amount, :tax, :description
     
     def net_amount
       amount - tax
